86c5a4d5db78bdc2a6d9c0dada7b4ea1270f8a65
luzzif <federico.luzzi@proton.me>
2026-06-24 22:54:05 +0100

    chore: remove unneeded permissions and bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "xyz.metiq"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.3.0"
+        versionCode = 6
+        versionName = "0.3.1"
 
         vectorDrawables {
             useSupportLibrary = true
