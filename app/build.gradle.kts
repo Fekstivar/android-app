c7848cdaa5dccfe7754b37c22cf80d960a1b6131
luzzif <federico.luzzi@proton.me>
2026-07-05 17:20:27 +0100

    chore: update fastlane metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "xyz.metiq"
         minSdk = 29
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.5.0"
+        versionCode = 10
+        versionName = "0.6.0"
 
         vectorDrawables {
             useSupportLibrary = true
