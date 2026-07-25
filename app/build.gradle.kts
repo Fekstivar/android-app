2733eb8daeae1a606e37ac4ad635b4f801441712
luzzif <federico.luzzi@proton.me>
2026-07-25 14:26:48 +0100

    chore: v0.10.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "xyz.metiq"
         minSdk = 28
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.10.0"
+        versionCode = 20
+        versionName = "0.10.1"
 
         vectorDrawables {
             useSupportLibrary = true
